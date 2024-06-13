c004e46841fd7666f72b083dc9e66836c07f7096
Ahsann-Ali <saltyswag372@gmail.com>
2024-06-13 13:43:23 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+let statment: string = "Your answer is";
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstNumber" },
+  { message: "Enter Second Number", type: "number", name: "secondNumber" },
+  { message: "Chose any operators",
+    type: "list",
+    name: "operators",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+// Conditional Statment (IF ELSE)
+
+if (answer.operators === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operators === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operators === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operators === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else console.log("please select any operator");
+
+// NPX Command.......................................................
+
+// -------------npx simple-calculator-by-ahsan-ali-------------------
